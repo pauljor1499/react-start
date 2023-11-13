b6b2ff83fa870a40424af5fd6edf672acd02af87
Paul Jor <jorpaulvincent@gmail.com>
2023-11-13 15:51:18 +0400

    update redux branch

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 //store
-//store
 import { Provider } from "react-redux";
 import { Store } from "./redux/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    //<React.StrictMode>
-    <Provider store={Store}>
-        <App />
-    </Provider>
-    //</React.StrictMode>
+    <React.StrictMode>
+        <Provider store={Store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
